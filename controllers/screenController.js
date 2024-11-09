ab1916d01711ec3fc7814f84034ec776a4757a05
axon04 <samj51590@gmail.com>
2024-11-09 12:41:42 +0530

    screen controller methods without validation added

--- controllers/screenController.js
+++ controllers/screenController.js
@@ -1,17 +1,80 @@
+const Screen = require("../models/screen");
+
 const createScreen = async (req, res) => {
-	res.send(req.body);
+	//create document
+	const newScreen = new Screen(req.body);
+	
+	try {
+		// save to database
+		const savedScreen = await newScreen.save();
+		
+		// return saved object
+		return res.status(201).json(savedScreen);
+	} catch (err) {
+		res.status(500).json({"error" : err});
+	}
 };
+
 const getAllScreens = async (req, res) => {
-	res.send("all screens");
+	try{
+		const allScreens = await Screen.find({});
+		return res.status(200).json(allScreens);
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({"error": "Server issue"})
+	}
 };
+
 const getScreenById = async (req, res) => {
-	res.send("get screen with id: " + req.params.id);
+	try{
+		const existingScreen = await Screen.findById(req.params.id);
+		// if no screen exists return
+		if (!existingScreen) { 
+			return res.status(400).json({"error": `No screen exists with id: ${req.params.id}`})
+		}
+		return res.status(200).json(existingScreen);
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({"error": "Server issue"})
+	}
 };
+
 const updateScreen = async (req, res) => {
-	res.send("update screen with id: " + req.params.id);
+	// get the object from body
+	const {name, seats, seatCount} = req.body;
+	// find the document
+	try {
+		const existingScreen = await Screen.findById(req.params.id);
+		// if not exists return
+		if (!existingScreen) {
+			return res.status(400).json({"error": `No screen exists with id: ${req.params.id}`});
+		}
+		existingScreen.name = name;
+		existingScreen.seats = seats;
+		existingScreen.seatCount = seatCount;
+		// save the document  
+		const updatedScreen = await existingScreen.save();
+		return res.status(200).json(updatedScreen);
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({"error": "Server issue"})
+	}
 };
+
 const deleteScreen = async (req, res) => {
-	res.send("delete screen with id: " + req.params.id);
+	// find the screen
+	try{
+		const existingScreen = await Screen.findById(req.params.id);
+		// if no screen exists return
+		if (!existingScreen) { 
+			return res.status(400).json({"error": `No screen exists with id: ${req.params.id}`})
+		}
+		await Screen.findByIdAndDelete(req.params.id);
+		return res.status(204).json({"message": "Deleted successfully"});
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({"error": "Server issue"})
+	}
 };
 
 module.exports = {
